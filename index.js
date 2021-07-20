4a6730c51fe19bcc5dc97d014c6386e25d55ffd2
Ryochi <ryo1.t.zubu@gmail.com>
2021-07-20 23:26:02 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(resp => resp.json())
+    .then(json => {
+        document.querySelector("body").innerHTML = `<div><ul><li>${json.id}</li></ul></div>`
+    })
+    .catch(error => {
+        alert("Something wrong");
+        console.log(error.message);
+        document.body.innerHTML = error;
+    });
+}
+
+submitData("Steve", "steve@steve.com")
